40435abde3797d3eb892a711760675a2ce06872b
Rajeev Ranjan <97228502+mrrajeev29@users.noreply.github.com>
2024-01-15 01:25:30 +0530

    Update Advice.js

--- src/component/Advice.js
+++ src/component/Advice.js
@@ -8,7 +8,9 @@ const advice=()=>{
         <div class="text-typing">
         <p id="i1">Hey, Are you getting Bored i have soluton for you. Try Our Quotes Generator that will make you laugh</p>
         </div>
-        
+        <div class="text-typing-mob">
+        <p id="i1">Hey,getting Bored? Check this out</p>
+        </div>
         <center><Link  to="sub"><button type="button" className="bn">Start</button></Link></center>
         <div class="text-typing2">
         <p>Created by Rajeev Ranjan. </p>

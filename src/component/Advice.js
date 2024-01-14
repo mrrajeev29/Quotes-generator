ca2da1f3140e713613afc3d7d867cf3583d67f85
Rajeev Ranjan <97228502+mrrajeev29@users.noreply.github.com>
2024-01-15 01:28:06 +0530

    Update Advice.js

--- src/component/Advice.js
+++ src/component/Advice.js
@@ -9,7 +9,7 @@ const advice=()=>{
         <p id="i1">Hey, Are you getting Bored i have soluton for you. Try Our Quotes Generator that will make you laugh</p>
         </div>
         <div class="text-typing-mob">
-        <p id="i1">Hey,getting Bored? Checkout this</p>
+        <p id="i1">Hey,getting Bored?Checkout this</p>
         </div>
         <center><Link  to="sub"><button type="button" className="bn">Start</button></Link></center>
         <div class="text-typing2">
